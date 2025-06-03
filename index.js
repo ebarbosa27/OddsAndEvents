7e7c24155843ddc3745b84993f18a3452f178f76
ebarbosa27 <ebarbosa5449@gmail.com>
2025-06-03 15:02:41 -0500

    Added some styling to the page so it looks cleaner

--- index.js
+++ index.js
@@ -31,9 +31,12 @@ function NumberForm() {
       Add a number to the bank
       <input name="number" />
       <button>Add number</button>
+      <button>Add Random</button>
     </label>
-    <button>Sort 1</button>
-    <button>Sort All</button>
+    <div class="functionButtons">
+      <button>Sort 1</button>
+      <button>Sort All</button>
+    </div>
   `;
 
   const buttonList = formElement.querySelectorAll("button");
@@ -53,14 +56,19 @@ function NumberForm() {
     addToBank(numberInput);
     render();
   });
-  // Sort only 1 number from bank to odd or even
   buttonList[1].addEventListener("click", (event) => {
+    event.preventDefault();
+    addToBank(Math.ceil(Math.random() * 99));
+    render();
+  });
+  // Sort only 1 number from bank to odd or even
+  buttonList[2].addEventListener("click", (event) => {
     event.preventDefault();
     moveNextNumber();
     render();
   });
   // Sort all numbers from bank to odd or even
-  buttonList[2].addEventListener("click", (event) => {
+  buttonList[3].addEventListener("click", (event) => {
     event.preventDefault();
     while (bankNums.length > 0) {
       moveNextNumber();
@@ -72,7 +80,7 @@ function NumberForm() {
 
 function BankComponent() {
   const bankElem = document.createElement("div");
-  bankElem.className = "";
+  bankElem.className = "numberShowcase";
   bankElem.innerHTML = `
     <h2>Bank</h2>
     <ul></ul>
@@ -88,7 +96,7 @@ function BankComponent() {
 
 function OddsComponent() {
   const oddsElem = document.createElement("div");
-  oddsElem.className = "";
+  oddsElem.className = "numberShowcase";
   oddsElem.innerHTML = `
     <h2>Odds</h2>
     <ul></ul>
@@ -104,7 +112,7 @@ function OddsComponent() {
 
 function EvensComponent() {
   const evenElem = document.createElement("div");
-  evenElem.className = "";
+  evenElem.className = "numberShowcase";
   evenElem.innerHTML = `
     <h2>Evens</h2>
     <ul></ul>
