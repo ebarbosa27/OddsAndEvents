0aea9ee19da3d4d3012df2edd1f6e2d218e18785
ebarbosa27 <ebarbosa5449@gmail.com>
2025-06-03 12:34:26 -0500

    Added a render function and filled in some content as well as place holder components for the rest of the page

---
+++ index.js
@@ -0,0 +1,23 @@
+function render() {
+  // Retrieve the app element from the html
+  const appElement = document.querySelector("#app");
+  // Fill in content and place holders for the rest of the html
+  appElement.innerHTML = html`
+    <h1>Odds and Events</h1>
+    <NumberForm></NumberForm>
+    <main>
+      <BankComponent></BankComponent>
+      <OddsComponent></OddsComponent>
+      <EvensComponent></EvensComponent>
+    </main>
+  `;
+
+  // Create the form at the top of page
+  appElement.querySelector("NumberForm").replaceWith("");
+
+  // Fill in the content under the form component
+  appElement.querySelector("BankComponent").replaceWith("");
+  appElement.querySelector("OddsComponent").replaceWith("");
+  appElement.querySelector("EvensComponent").replaceWith("");
+}
+render();
